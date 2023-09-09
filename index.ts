03fefeef6887552f0a64c5f2311f43fec952a3af
TellMin <dhgirkcls@gmail.com>
2023-09-10 00:06:50 +0900

    Add synchronous figlet font loading and type definition

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import figlet from "figlet";
+import standard from "figlet/importable-fonts/Standard";
 import { Hono } from "hono";
 
 const app = new Hono();
 
+figlet.parseFont("Standard", standard);
 const body = figlet.textSync("Bun!");
 
 app.get("/", (c) => c.text(body));
---
+++ types/figlet.d.ts
@@ -0,0 +1,4 @@
+declare module "figlet/importable-fonts/Standard" {
+  const font: string;
+  export = font;
+}
